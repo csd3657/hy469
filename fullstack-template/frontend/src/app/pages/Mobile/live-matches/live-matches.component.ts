72307b5134df042c7b04e6a13f41c7867d76ee30
Tonia Psomadaki <csd3878@csd.uoc.gr>
2019-11-19 13:30:11 +0200

    added menu component for mobile

---
+++ fullstack-template/frontend/src/app/pages/Mobile/live-matches/live-matches.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'live-matches',
+  templateUrl: './live-matches.component.html',
+  styleUrls: ['./live-matches.component.scss'],
+  encapsulation: ViewEncapsulation.Native
+})
+export class liveMatchesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ fullstack-template/frontend/src/app/pages/Mobile/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+  encapsulation: ViewEncapsulation.Native
+})
+export class menuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
