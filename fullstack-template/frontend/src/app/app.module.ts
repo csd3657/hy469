b5146bf818e69f74fde249befc61bb45b29f5c73
csd3657 <csd3657@csd.uoc.gr>
2019-11-19 20:39:56 +0200

    Add Table Components

--- fullstack-template/frontend/src/app/app.module.ts
+++ fullstack-template/frontend/src/app/app.module.ts
@@ -18,12 +18,9 @@ import { VotestatsComponent } from './pages/AugmTable/votestats/votestats.compon
 import { Votestep1Component } from './pages/AugmTable/votestep1/votestep1.component';
 import { Votestep2Component } from './pages/AugmTable/votestep2/votestep2.component';
 import { LiveinfosComponent } from './pages/AugmTable/liveinfos/liveinfos.component';
-<<<<<<< HEAD
 import { SubstitutePlayerComponent } from './pages/AugmTable/liveinfos/substitute-player/substitute-player.component';
 import { StartingPlayerComponent } from './pages/AugmTable/liveinfos/starting-player/starting-player.component';
-=======
-import { LiveMatchComponent } from './pages/TV/live-match/live-match.component';
->>>>>>> c5884b4430c4001d0a52fae87ce9a2caedf840f9
+import { SubstitutePlayerBottomComponent } from './pages/AugmTable/liveinfos/substitute-player-bottom/substitute-player-bottom.component';
 
 @NgModule({
   declarations: [
@@ -41,12 +38,9 @@ import { LiveMatchComponent } from './pages/TV/live-match/live-match.component';
     Votestep1Component,
     Votestep2Component,
     LiveinfosComponent,
-<<<<<<< HEAD
     SubstitutePlayerComponent,
-    StartingPlayerComponent
-=======
-    LiveMatchComponent
->>>>>>> c5884b4430c4001d0a52fae87ce9a2caedf840f9
+    StartingPlayerComponent,
+    SubstitutePlayerBottomComponent
   ],
   imports: [
     BrowserModule,
