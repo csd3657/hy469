b76f26a75a8d91d84845ae034294526316781d50
Gymnasio Vamou <44649399+gymvamou@users.noreply.github.com>
2019-11-22 21:36:40 +0200

    newVideosTvinProgress

--- fullstack-template/frontend/src/app/app.module.ts
+++ fullstack-template/frontend/src/app/app.module.ts
@@ -9,10 +9,6 @@ import { AppRoutingModule } from './app.routing';
 import { SidebarMenuComponent } from './pages/TV/sidebar-menu/sidebar-menu.component';
 import { HomepageTVComponent } from './pages/TV/homepage-tv/homepage-tv.component';
 import { VideoTVComponent } from './pages/TV/video-tv/video-tv.component';
-import { TitleTagComponent } from './pages/TV/title-tag/title-tag.component';
-import { VideosRowComponent } from './pages/TV/videos-row/videos-row.component';
-import { VideosRow1Component } from './pages/TV/videos-row1/videos-row1.component';
-import { VideosRow3Component } from './pages/TV/videos-row3/videos-row3.component';
 import { VotestatsComponent } from './pages/AugmTable/votestats/votestats.component';
 import { Votestep1Component } from './pages/AugmTable/votestep1/votestep1.component';
 import { Votestep2Component } from './pages/AugmTable/votestep2/votestep2.component';
@@ -25,10 +21,10 @@ import { SubstitutePlayerComponent } from './pages/AugmTable/liveinfos/substitut
 import { StartingPlayerComponent } from './pages/AugmTable/liveinfos/starting-player/starting-player.component';
 import { SubstitutePlayerBottomComponent } from './pages/AugmTable/liveinfos/substitute-player-bottom/substitute-player-bottom.component';
 import { WatchlistTvComponent } from './pages/TV/watchlist-tv/watchlist-tv.component';
-import { TitleTagWatchComponent } from './pages/TV/title-tag-watch/title-tag-watch.component';
 import { ScheduleTVComponent } from './pages/TV/schedule-tv/schedule-tv.component';
 import { FirstMatchComponent } from './pages/TV/first-match/first-match.component';
 import { UpcomingMatchComponent } from './pages/TV/upcoming-match/upcoming-match.component';
+import { TitleTagComponent } from './title-tag/title-tag.component';
 
 
 @NgModule({
@@ -37,10 +33,6 @@ import { UpcomingMatchComponent } from './pages/TV/upcoming-match/upcoming-match
     SidebarMenuComponent,
     HomepageTVComponent,
     VideoTVComponent,
-    TitleTagComponent,
-    VideosRowComponent,
-    VideosRow1Component,
-    VideosRow3Component,
     VotestatsComponent,
     Votestep1Component,
     Votestep2Component,
@@ -50,10 +42,10 @@ import { UpcomingMatchComponent } from './pages/TV/upcoming-match/upcoming-match
     LiveMatchComponent,
     SubstitutePlayerBottomComponent,
     WatchlistTvComponent,
-    TitleTagWatchComponent,
     ScheduleTVComponent,
     FirstMatchComponent,
-    UpcomingMatchComponent
+    UpcomingMatchComponent,
+    TitleTagComponent
   ],
   imports: [
     BrowserModule,
