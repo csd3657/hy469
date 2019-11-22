ba211b473abae9aa0bf55574458daa28d843bca7
Smyrn96 <csd3504@csd.uoc.gr>
2019-11-22 11:31:22 +0200

    error

--- fullstack-template/frontend/src/app/app.module.ts
+++ fullstack-template/frontend/src/app/app.module.ts
@@ -8,9 +8,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { SidebarMenuComponent } from './pages/TV/sidebar-menu/sidebar-menu.component';
 import { HomepageTVComponent } from './pages/TV/homepage-tv/homepage-tv.component';
-import { LiveGamesComponent } from './pages/TV/live-games/live-games.component';
 import { VideoTVComponent } from './pages/TV/video-tv/video-tv.component';
-import { MenuIndicatorComponent } from './pages/TV/menu-indicator/menu-indicator.component';
 import { TitleTagComponent } from './pages/TV/title-tag/title-tag.component';
 import { VideosRowComponent } from './pages/TV/videos-row/videos-row.component';
 import { VideosRow1Component } from './pages/TV/videos-row1/videos-row1.component';
@@ -36,9 +34,7 @@ import { ScheduleTVComponent } from './pages/TV/schedule-tv/schedule-tv.componen
     AppComponent,
     SidebarMenuComponent,
     HomepageTVComponent,
-    LiveGamesComponent,
     VideoTVComponent,
-    MenuIndicatorComponent,
     TitleTagComponent,
     VideosRowComponent,
     VideosRow1Component,
