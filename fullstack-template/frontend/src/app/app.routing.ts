38c1393ca17ab206c242b467b183530f7997afc2
csd3657 <csd3657@csd.uoc.gr>
2019-12-09 16:24:41 +0200

    continue

--- fullstack-template/frontend/src/app/app.routing.ts
+++ fullstack-template/frontend/src/app/app.routing.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageTVComponent } from './pages/TV/homepage-tv/homepage-tv.component';
 import { VideoTVComponent } from './pages/TV/video-tv/video-tv.component';
-import { VotestatsComponent } from './pages/AugmTable/voting/votestats/votestats.component';
 
 
 
@@ -12,7 +11,6 @@ import { SubstitutePlayerComponent } from './pages/AugmTable/liveinfos/substitut
 import { StartingPlayerComponent } from './pages/AugmTable/liveinfos/starting-player/starting-player.component';
 
 
-import { LiveMatchComponent } from './pages/TV/live-match/live-match.component';
 import { WatchlistTvComponent } from './pages/TV/watchlist-tv/watchlist-tv.component';
 import { ScheduleTVComponent } from './pages/TV/schedule-tv/schedule-tv.component';
 import { VotingComponent } from './pages/AugmTable/voting/voting.component';
