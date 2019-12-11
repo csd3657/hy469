d06410978dd1877fcb21e46d488078b75ab987a8
csd3657 <csd3657@csd.uoc.gr>
2019-12-11 20:51:57 +0200

    Add example serve

--- fullstack-template/frontend/src/app/pages/TV/homepage-tv/homepage-tv.component.ts
+++ fullstack-template/frontend/src/app/pages/TV/homepage-tv/homepage-tv.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SocketsService } from 'src/app/global/services';
+import { MobileService } from 'src/app/global/services/mobile/mobile.service';
 
 @Component({
   selector: 'ami-fullstack-homepage-tv',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageTVComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _socketService: SocketsService, private _mobileService: MobileService) { }
 
   ngOnInit() {
+    this._socketService.syncMessages("openMatch").subscribe(msg=>{
+      console.log(msg)
+      if(msg.message["show"]){
+        document.getElementById("theMatch").style.display="block";
+      }else{
+        document.getElementById("theMatch").style.display="none"
+      }
+    })
+  }
+
+  closeMatch(){
+    this._mobileService.closeMatchToTv(false).subscribe();
   }
 
 }
