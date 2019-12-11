a8b6c1ccd9950d2121ebc202ce2159741c537a69
Smyrn96 <csd3504@csd.uoc.gr>
2019-12-11 14:28:14 +0200

    Wrong Commit

---
+++ fullstack-template/frontend/src/app/pages/Mobile/specific-news/specific-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ami-fullstack-specific-news',
+  templateUrl: './specific-news.component.html',
+  styleUrls: ['./specific-news.component.scss']
+})
+export class SpecificNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
