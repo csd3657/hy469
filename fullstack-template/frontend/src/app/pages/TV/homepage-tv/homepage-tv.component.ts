a8772073d9471bc381d3264d1dda34f2efea5c67
Smyrn96 <csd3504@csd.uoc.gr>
2019-12-12 12:33:29 +0200

    Scenario1

--- fullstack-template/frontend/src/app/pages/TV/homepage-tv/homepage-tv.component.ts
+++ fullstack-template/frontend/src/app/pages/TV/homepage-tv/homepage-tv.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketsService } from 'src/app/global/services';
 import { MobileService } from 'src/app/global/services/mobile/mobile.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'ami-fullstack-homepage-tv',
@@ -9,15 +10,17 @@ import { MobileService } from 'src/app/global/services/mobile/mobile.service';
 })
 export class HomepageTVComponent implements OnInit {
 
-  constructor(private _socketService: SocketsService, private _mobileService: MobileService) { }
+  constructor(private _socketService: SocketsService, private _mobileService: MobileService,private router: Router) { }
 
   ngOnInit() {
     this._socketService.syncMessages("openMatch").subscribe(msg=>{
       console.log(msg)
       if(msg.message["show"]){
-        document.getElementById("theMatch").style.display="block";
+        //document.getElementById("theMatch").style.display="block";
+        this.router.navigateByUrl('/RealVsMaccabi');
       }else{
         document.getElementById("theMatch").style.display="none"
+        this.router.navigateByUrl('/HomepageTv');
       }
     })
   }
