7f9f8340e7ad9ca5f9c0452f51e12c81ed5f68a7
csd3657 <csd3657@csd.uoc.gr>
2019-12-18 20:08:58 +0200

    Diorthosi sto table stats json

--- fullstack-template/frontend/src/app/pages/AugmTable/liveinfos/liveinfos.component.ts
+++ fullstack-template/frontend/src/app/pages/AugmTable/liveinfos/liveinfos.component.ts
@@ -7,25 +7,33 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LiveinfosComponent implements OnInit {
 
-  @Input() team1;
-  @Input() team2;
+
+  @Input() team1: any;
+  @Input() team2: any;
 
 
   constructor() {
+    this.team1 =
+    {
+      score: 0,
+      "player1": { pts: 0, ass: 0, reb: 0 },
+      "player2": { pts: 0, ass: 0, reb: 0 },
+      "player3": { pts: 0, ass: 0, reb: 0 },
+      "player4": { pts: 0, ass: 0, reb: 0 },
+      "player5": { pts: 0, ass: 0, reb: 0 }
+    };
 
-    this.team1 = {
+    this.team2 =
+    {
       score: 0,
-      pts  : 0,
-      ass  : 0,
-      reb  : 0
-    }
-    this.team2 = {
-      score: 0,
-      pts  : 0,
-      ass  : 0,
-      reb  : 0
-    }
-   }
+      "player1": { pts: 0, ass: 0, reb: 0 },
+      "player2": { pts: 0, ass: 0, reb: 0 },
+      "player3": { pts: 0, ass: 0, reb: 0 },
+      "player4": { pts: 0, ass: 0, reb: 0 },
+      "player5": { pts: 0, ass: 0, reb: 0 }
+    };
+
+  }
 
   ngOnInit() {
   }
