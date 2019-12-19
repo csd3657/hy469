2e4ab1c2691884e3643433e9659d0bc857df34a2
csd3657 <csd3657@csd.uoc.gr>
2019-12-19 18:31:17 +0200

    add table voting functionality

--- fullstack-template/frontend/src/app/pages/AugmTable/voting/votestep1/votestep1.component.ts
+++ fullstack-template/frontend/src/app/pages/AugmTable/voting/votestep1/votestep1.component.ts
@@ -6,15 +6,55 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./votestep1.component.scss']
 })
 export class Votestep1Component implements OnInit {
+  public clicked = false;
 
+  public player;
   @Output() public childEvent = new EventEmitter();
+  @Output() public image_vote = new EventEmitter();
 
   constructor() { }
 
-  SendToParent(){
-    this.childEvent.emit();
+  save_image(){
+    this.image_vote.emit(this.player);
+  }
+  SendToParent() {
+    if(this.clicked){
+      this.save_image();
+      this.childEvent.emit();
+    }
+    
   }
   ngOnInit() {
+  }
+
+
+  player_clicked(player) {
+    this.player = player;
+    if (player === 'campazzo') {
+      if (!this.clicked) {
+        document.getElementById("campazzo").style.border = "3px solid white";
+        document.getElementById("dorsey").style.border = "0px solid white";
+        this.clicked = true;
+      } else {
+        this.clicked = false;
+        document.getElementById("campazzo").style.border = "0px solid white";
+      }
+
+
+    }
+
+    if (player == 'dorsey') {
+      if (!this.clicked) {
+        document.getElementById("campazzo").style.border = "0px solid white";
+        document.getElementById("dorsey").style.border = "3px solid white";
+        this.clicked = true;
+      } else {
+        this.clicked = false;
+        document.getElementById("dorsey").style.border = "0px solid white";
+      }
+
+    }
+
   }
 
 }
--- fullstack-template/frontend/src/app/pages/AugmTable/voting/votestep3/votestep3.component.ts
+++ fullstack-template/frontend/src/app/pages/AugmTable/voting/votestep3/votestep3.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ami-fullstack-votestep3',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./votestep3.component.scss']
 })
 export class Votestep3Component implements OnInit {
-
-  constructor() { }
+  @Output()  closeVote = new EventEmitter();
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  SendToParent(){
+    this.closeVote.emit();
   }
 
 }
